9c5294835b90c2649cec31421f2b100abc00984f
saieeash Reddy <saieeashreddy1@gmail.com>
2024-12-28 20:23:32 +0530

    actions file is added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('hello world')
+console.log(
+  'This is a test script file that will be uploaded to s3 and from there we access using amazon cloudFront'
+)
+
+console.log('This is changed and updated ')
+console.log('This is again updated')
