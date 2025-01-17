de42c1b9c9acc574e52cd5bc61ddf929cedc2678
saieeash Reddy <saieeashreddy1@gmail.com>
2025-01-17 14:40:18 +0530

    new workflow added

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ console.log(
   'This is a test script file that will be uploaded to s3 and from there we access using amazon cloudFront'
 )
 
-console.log('This is changed and updated ')
-console.log('Now file uploaded using role oidc')
-console.log('This is My finual udpated')
+console.log(
+  'This is my latest script with cache invalidation with lamda function'
+)
