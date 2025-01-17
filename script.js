2613865d23c27e475822b43f5eee044cfba9af6f
saieeash Reddy <saieeashreddy1@gmail.com>
2025-01-17 14:52:11 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 console.log('hello world')
-console.log(
-  'This is a test script file that will be uploaded to s3 and from there we access using amazon cloudFront'
-)
-
-console.log(
-  'This is my latest script with cache invalidation with lamda function'
-)
